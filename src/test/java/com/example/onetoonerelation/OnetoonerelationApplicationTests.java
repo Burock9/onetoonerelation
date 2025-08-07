ed08d0d7fb30f50737c7d608bd30d673c06db169
Burock9 <burakkocas@hotmail.com>
2025-08-07 10:03:29 +0300

    Initial commit

---
+++ src/test/java/com/example/onetoonerelation/OnetoonerelationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onetoonerelation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnetoonerelationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
